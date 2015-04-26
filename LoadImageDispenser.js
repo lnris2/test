3399df55f482b05e557b014311ee75071bb181c0
lnris2 <gkillerplay@outlook.com>
2015-04-26 13:18:18 -0300

    Create LoadImageDispenser.js

---
+++ LoadImageDispenser.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        ImageLoadDispenser
+// @namespace  
+// @description Load Images
+// @include     *dispenser.tf*
+// @version     1
+// @author      .
+// ==/UserScript==
+render_inventory_img($("canvas"))
