9b22d52961795a2da93f6467c9957c1226d4d9b1
lnris2 <gkillerplay@outlook.com>
2015-04-26 13:21:57 -0300

    Update LoadImageDispenser.js

--- LoadImageDispenser.js
+++ LoadImageDispenser.js
@@ -3,6 +3,7 @@
 // @namespace  
 // @description Load Images
 // @include     *dispenser.tf*
+// @downloadURL	https://raw.githubusercontent.com/lnris2/test/master/LoadImageDispenser.js
 // @version     1
 // @author      .
 // ==/UserScript==
